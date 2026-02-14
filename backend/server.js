ab54275c72be0a50a7e071586f8ee36502aa84f7
Affan Nabeel <affan@technassolution.com>
2026-02-14 22:15:25 +0530

    DB Chnages

--- backend/server.js
+++ backend/server.js
@@ -9,9 +9,12 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI)
-    .then(() => console.log("MongoDB Connected"))
-    .catch(err => console.log(err));
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+.then(() => console.log("MongoDB connected ✅"))
+.catch(err => console.log("MongoDB connection error ❌", err));
 
 app.use("/api/auth", require("./routes/authRoutes"));
 app.use("/api/tasks", require("./routes/taskRoutes"));
